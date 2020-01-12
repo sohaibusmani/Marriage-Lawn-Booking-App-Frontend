cdb2fdefc18f25c5fb20bab797a7011008485b7b
sohaibusmani <sohaibusmani52@gmail.com>
2020-01-12 15:34:31 -0800

    Landing Page UI commit 1

---
+++ src/Config/Firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "api-key",
+    authDomain: "project-id.firebaseapp.com",
+    databaseURL: "https://project-id.firebaseio.com",
+    projectId: "project-id",
+    storageBucket: "project-id.appspot.com",
+    messagingSenderId: "sender-id",
+    appId: "app-id",
+    measurementId: "G-measurement-id",
+  };
+
+  firebase.initializeApp(firebaseConfig)
+
+  export default firebase;
--- src/Screens/LandingPage/LandingPage.js
+++ src/Screens/LandingPage/LandingPage.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { Container, Row, Col, Card,Button } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+
+
+// Images
+import pic1 from '../../Assets/Images/download.jpg'
 
 
 class LandingPage extends React.Component {
@@ -12,7 +16,14 @@ class LandingPage extends React.Component {
                 <Container>
                     <Row>
                         <Col>
-                          <h1>Landing Page</h1>
+                            <Card style={{width:'85rem', marginTop:'1rem', marginLeft:'-130px', height:'20rem', border:'solid 2px #ff0147' }}>
+                                
+                                <Card.Body>
+                                    <Card.Title style={{textAlign:'center', marginTop:'100px', fontFamily:'sens-serif' }}><h1>Lawn Booking App</h1></Card.Title>
+                                    
+                                    
+                                </Card.Body>
+                            </Card>
                         </Col>
                     </Row>
                 </Container>
